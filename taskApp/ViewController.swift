4ef5cb4518de1aa32e2a4c3473bd83c1160156aa
tsujimura <tsujimura@takachiho-sc.co.jp>
2022-06-07 14:15:40 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by kaihatsu on 2022/06/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
